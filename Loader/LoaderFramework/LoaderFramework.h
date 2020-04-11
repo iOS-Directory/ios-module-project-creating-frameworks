b62c119a10537201a4d93569228649995fc6a629
fritzgt <tuanis@live.com>
2020-04-11 16:43:12 +0300

    create framework and added to dependencies

---
+++ Loader/LoaderFramework/LoaderFramework.h
@@ -0,0 +1,19 @@
+//
+//  LoaderFramework.h
+//  LoaderFramework
+//
+//  Created by FGT MAC on 4/11/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoaderFramework.
+FOUNDATION_EXPORT double LoaderFrameworkVersionNumber;
+
+//! Project version string for LoaderFramework.
+FOUNDATION_EXPORT const unsigned char LoaderFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoaderFramework/PublicHeader.h>
+
+
